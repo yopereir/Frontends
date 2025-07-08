0661d80d776682b47fd7d24558860552876a1889
Yohann Pereira <ypereira@bgea.org>
2025-07-08 14:14:45 -0400

    added sitemap

--- React/company/vite.config.js
+++ React/company/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -13,5 +13,19 @@ export default defineConfig({
         }
       },
     },
+    Sitemap({
+      hostname: 'https://www.your-domain.com',
+      dynamicRoutes: [
+        '/',
+        '/about',
+        '/contact',
+        '/jobrequest',
+        '//businessintelligence',
+        '/projectlist',
+        '/projects/road-rage',
+        '/projects/socratic-discussion',
+        '/projects/elf-bowling',
+      ],
+    }),
   ],
-})
+});
